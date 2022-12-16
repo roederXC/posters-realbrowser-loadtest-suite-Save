9400fe2406f121ccc988b5cb03d318ea79344e5c
Philipp_Roeder <p.roeder@xceptance.net>
2022-12-16 13:24:18 +0100

    added a validation for configure size

--- src/main/java/com/xceptance/loadtest/posters/pages/ProductDetailsPage.java
+++ src/main/java/com/xceptance/loadtest/posters/pages/ProductDetailsPage.java
@@ -1,7 +1,11 @@
 package com.xceptance.loadtest.posters.pages;
 
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertTrue;
+import java.util.List;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import static com.codeborne.selenide.Condition .*;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
@@ -38,27 +42,45 @@ public class ProductDetailsPage
 	
 	public static void configureProductSize()
 	{
-	    ElementsCollection sizeOtions = $$("#selectSize > option").filter(visible);
-	    if(!sizeOtions.isEmpty())
+	    // get the select element
+	    SelenideElement selectElement = $("#selectSize").should(visible);
+	    
+	    //convert it to Select and get all options
+	    Select select = new Select(selectElement);
+	    List<WebElement> options = select.getOptions();
+	    
+	    // get the current price. We need this later for validation
+	    String oldPrice = $("#prodPrice").getText();
+	    
+	    // remove all selected Options
+	    options.removeIf(o -> o.isSelected());	    
+	    
+	    // click one of the not selected options
+	    if(!options.isEmpty())
 	    {
 	        Action.run("ConfigureProductSize", () ->
 	        {
-	            final SelenideElement randomOption = RandomUtils.randomEntry(sizeOtions);
+	            final WebElement randomOption = RandomUtils.randomEntry(options);
 	            randomOption.click();
 	        });
+	        
+	        // validate that the price has changed
+	        String newPrice = $("#prodPrice").getText();
+	        assertTrue(oldPrice != newPrice);
 	    }
 	}	
 	
 	public static void configureProductStyle()
-	{
-	    ElementsCollection styleOtions = $$("#selectStyle input:not([checked])").filter(visible);
-	    
-	    if(!styleOtions.isEmpty())
+	{ 
+	    // get all unselected styles
+	    ElementsCollection styleOptions = $$("#selectStyle input:not([checked])").filter(visible);
+	    	    
+	    if(!styleOptions.isEmpty())
 	    {
 	        Action.run("ConfigureProductStyle", () ->
-	        {
-	            final SelenideElement randomOption = RandomUtils.randomEntry(styleOtions);
-	            randomOption.click();	                
+	        {	            
+	            final SelenideElement randomOption = RandomUtils.randomEntry(styleOptions);
+	            randomOption.click();
 	        });
 	    }
 	}		
